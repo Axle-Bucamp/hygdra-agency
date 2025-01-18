5f7dcdd8890ad6b97a8bf03bf502954275513ca5
Axle-Bucamp <axle.bucamp@gmail.com>
2025-01-18 05:47:30 -0500

    update dev agent, better task management

--- Backend/app/HygdraAgency/Agent/DeveloperAgent.py
+++ Backend/app/HygdraAgency/Agent/DeveloperAgent.py
@@ -10,6 +10,7 @@ from HygdraAgency.DataModel.Service import Service, CodeFile
 from HygdraAgency.Agent.BaseAgent import BaseAgent
 from HygdraAgency.Agent.Ollama import OllamaClient, OllamaModelConfig, OllamaPrompt, OllamaResponse
 from HygdraAgency.utils.rag import retrieve, Deps, Document, store_document
+import re
 
 # --- Enhanced Developer Agent with Code Generation ---
 class DeveloperAgent(BaseAgent):
@@ -19,125 +20,130 @@ class DeveloperAgent(BaseAgent):
     
     # add a continue fonction with a loop system thought
     async def work_on_task(self, task: Task, project:Project) -> dict:
-        context = project.description
         async with OllamaClient(self.ollama_config) as ollama:
             # Generate implementation plan
             plan_prompt = OllamaPrompt(
                 prompt=f"""Task: {task.title}
                 Description: {task.description}
-                Which external ressources might be requiered ?
-                Context: {context}""",
+                Which external ressources might be requiered to fullfill this task ?
+                Context: {project.description}""",
                 system="You are a senior software developer. Create a detailed plan."
             )
 
             external_ressources = await ollama.generate(plan_prompt)
             external_ressources = await retrieve(Deps(project, 5), external_ressources)
 
+            # do the loop and use regex ?
             plan_prompt = OllamaPrompt(
                 prompt=f"""Task: {task.title}
                 Description: {task.description}
+                Context: {project.description}
+                external ressources : {external_ressources}
+                ---- request: 
                 Create a detailed implementation plan.
-                Decompose those task into different code file.
-                Context: {context}
-                external ressources : {external_ressources}
-                ---- Important: 
-                return only the task list.
-                use this CSV format:
-                task1-name, task1-description, file1-name, coding langage
-                task2-name, task2-description, file2-name, coding langage
-                task3-name, task3-description, file3-name, coding langage
+                Decompose the task into different code file.
+                return file name, small descriptiom, 
+                functions header and coding langage into an actionnable list.
                 """,
                 system="You are a senior software developer. Create a detailed plan."
             )
+
             implementation_plan = await ollama.generate(plan_prompt)
-            print(implementation_plan)
 
-            plan_prompt = OllamaPrompt(
-                prompt=f"""Task: {task.title}
-                Description: {task.description}
-                generate the functions headers you will need to code.
-                Context: {context}
-                external ressources : {external_ressources}
-                plan : 
-                {implementation_plan}
-                """,
-                system="You are a senior software developer. Create a detailed plan."
-            )
-            function_header = await ollama.generate(plan_prompt)
+            # generate file
+            i=0
             code = []
-            for i, line in enumerate(implementation_plan.split("\n")):
-                task_infos = line.split(",") # task_name, task_description, file_name, langage
-                if len(task_infos) == 4:
-                    print(task_infos)
-                    plan_prompt = OllamaPrompt(
-                        prompt=f"""Task: {task_infos[0]}
-                        Description: {task_infos[1]}
-                        Which ressources might be requiered to code this task ?
-                        future function header : 
-                        {function_header}
-                        external ressources :
-                        {external_ressources}
-                        Context: {context}""",
-                        system="You are a senior software developer. Create a detailed plan."
-                    )
-                    external_ressources = await ollama.generate(plan_prompt)
-                    external_ressources = await retrieve(Deps(project, 5), external_ressources)
+            while i < 20:
+                plan_prompt = OllamaPrompt(
+                    prompt=f"""Task: {task.title}
+                    Description: {task.description}
+                    Context: {project.description}
+                    external ressources : {external_ressources}
+                    plan : 
+                    {implementation_plan}
+                    --- request :
+                    select the next file to code.
+                    extract the function header,
+                    describe it.
+                    choose a langage that suit the request.
+                    """,
+                    system="You are a senior software developer. Create a detailed plan."
+                )
+                next_file = await ollama.generate(plan_prompt)
+                external_ressources = await retrieve(Deps(project, 5), next_file)
 
-                    # Generate code
-                    code_prompt = OllamaPrompt(
-                        prompt=f"""Task: {task_infos[0]}
-                        Description: {task_infos[1]}
-                        context : {context}
-                        Langage : {task_infos[3]}
-                        --- note :
-                        Generate the implementation code.
-                        ensure the code quality.
-                        Generalise key concept into function.
-                        --- important :
-                        return only the generated code as one main file.
-                        external ressources : 
-                        {external_ressources}""",
-                        system="You are an expert programmer. Generate production-quality code. You prefer python"
-                    )
-                    generated_code = await ollama.generate(code_prompt)
+                plan_prompt = OllamaPrompt(
+                    prompt=f"""Task: {task.title}
+                    Description: {task.description}
+                    Context: {project.description}
+                    external ressources: {external_ressources}
+                    todo code : 
+                    {next_file}
+                    --- request :
+                    As an expert you need to code those function and fit them into one file.
+                    stucture the request as markdown.
+                    """,
+                    system="You are a senior software developer. Create a detailed plan."
+                )
 
-                    # Generate code
-                    code_prompt = OllamaPrompt(
-                        prompt=f"""Task: {task_infos[0]}
-                        Description: {task_infos[1]}
-                        Plan: {implementation_plan}
-                        first part code : {generated_code}
-                        Return only the name of the code file.
-                        external ressources : 
-                        {external_ressources}""",
-                        system="You are an expert programmer. Generate production-quality code. You prefer python."
-                    )
-                    generated_code = await ollama.generate(code_prompt)
+                code_file = await ollama.generate(plan_prompt)
 
-                    #plus propre plus tard
-                    os.mkdir(f"/home/guidry/Documents/agency/hygdra-agency/Backend/app/generated_code/{project.id}") 
-                    os.mkdir(f"/home/guidry/Documents/agency/hygdra-agency/Backend/app/generated_code/{project.id}/{task.title}")
-                    with open(f"/home/guidry/Documents/agency/hygdra-agency/Backend/app/generated_code/{project.id}/{task.title}/{task_infos[2]}" , 'w') as file:
-                        file.write(generated_code)
+                plan_prompt = OllamaPrompt(
+                    prompt=f"""Task: {task.title}
+                    Description: {task.description}
+                    Context: {project.description}
+                    external ressources: {external_ressources}
+                    {code_file}
+                    --- request :
+                    what is the name of this code file with extension.
+                    """,
+                    system="You are a senior software developer. Create a detailed plan."
+                )
 
-                    store_document(Deps(project, 5), Document(task_infos[2],task_infos[2],generated_code))
-                    codefile = CodeFile(id=task_infos[0], name=task_infos[0], description=task_infos[1], filename=task_infos[2], status="coded", langage=task_infos[3])
-                    code.append(codefile)
+                filename = await ollama.generate(plan_prompt)
+                match = re.search(r'([^/\\]+)\.(py|js|java|cpp|html|css|rb|go|ts|php)$', filename)
+                if match:
+                    filename = match.group(1)
+                else:
+                    filename=hash(filename)
+
+                pattern = r'^```(?:\w+)?\s*\n(.*?)(?=^```)```'
+                result = re.findall(pattern, code_file, re.DOTALL | re.MULTILINE)
+                code_file = "\\n".join(result)
                 
-                    # Generate tests
-                    test_prompt = OllamaPrompt(
-                        prompt=f"Generate unit tests for this code:\n{generated_code}",
-                        system="You are a QA engineer. Generate comprehensive tests."
-                    )
+                #plus propre plus tard
+                os.mkdir(f"/home/guidry/Documents/agency/hygdra-agency/Backend/app/generated_code/{project.id}") 
+                os.mkdir(f"/home/guidry/Documents/agency/hygdra-agency/Backend/app/generated_code/{project.id}/{task.title}")
 
-                    generated_tests = await ollama.generate(test_prompt)
-                    with open(f"/home/guidry/Documents/agency/hygdra-agency/Backend/app/generated_code/{project.id}/{task.title}/test_{task_infos[2]}", 'w') as file:
-                        file.write(generated_tests)
+                with open(f"/home/guidry/Documents/agency/hygdra-agency/Backend/app/generated_code/{project.id}/{task.title}/{filename}" , 'w') as file:
+                    file.write(code_file)
 
-                    # task_name, task_description, file_name, langage
-            
-                    store_document(Deps(project, 5), Document("test_" + task_infos[2],"test_" + task_infos[2],generated_tests))
-                    print("new code added")
+                store_document(Deps(project, 5), Document(filename, next_file ,code_file))
+                codefile = CodeFile(id=filename, name=filename, description=next_file, filename=filename, status="coded", langage=filename)
+                code.append(codefile)
+
+                plan_prompt = OllamaPrompt(
+                    prompt=f"""Task: {task.title}
+                    Description: {task.description}
+                    Context: {project.description}
+                    plan : 
+                    {implementation_plan}
+
+                    -- task Done:
+                    {next_file}
+                    --- request:
+                    mark the task as done in the plan.
+                    update the plan.
+                    --- important:
+                    if the plan is fully implemented then return 'BREAK'
+                    """,
+                    system="You are a senior software developer. Create a detailed plan."
+                )
+                implementation_plan = await ollama.generate(plan_prompt)
+
+                if "BREAK" in str(implementation_plan).upper():
+                    break
+                i+=1
 
             task.status = TaskStatus.DONE
             service = Service(id=task.title, name=task.title, doc=task.description, status="coded", description=task.description, code=code)
